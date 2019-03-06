a88e5feb211323374c1466135f9f910ba96e8605
Michaël Vauthey <mickyvauthey@gmail.com>
2019-03-06 18:02:44 +0800

    fixed cloud version

--- config.py
+++ config.py
@@ -5,15 +5,18 @@ import os
 """
 NOTE:
 - this config file stores global parameters
+
+TODO:
+- print parameters used
 """
 
 # params
 params = {
 	'debug': True,  # debug mode (uses less images)
 	'cloud': False,  # if script to be run on paperspace.com using GPU
-	'val_size': 0.2,
+	'val_size': 0.1,
 	'batch_size': 32,
-	'epochs': 2,
+	'epochs': 1,
 	'patience': 10,
 	'debug_train_size': 32*20,
 	'debug_val_size': 32*2,
@@ -23,39 +26,28 @@ params = {
 # folders
 ROOT = os.path.abspath('')
 OUTPUT = os.path.join(ROOT, 'output')
-TRAIN = os.path.join(ROOT, 'temp', 'train')
-VAL = os.path.join(ROOT, 'temp', 'val')
-TEST = os.path.join(ROOT, 'input', 'test')
+INPUT = os.path.join(ROOT, 'input')
 
 # constants
 IMG_SIZE = 96
 
-# make sure train set and val set are a multiple of batch size
-# Keras ImageDataGenerator doesn't behave properly otherwise
-N = 220025
-n_batches = np.floor(N / params['batch_size'])
-n_batches_train = np.round(n_batches * (1-params['val_size']))
-n_batches_val = n_batches - n_batches_train
-N_train = int(n_batches_train * params['batch_size'])
-N_val = int(n_batches_val * params['batch_size'])
-
 # adjust some parameters if in debug mode
 if params['debug']==True:
-	TRAIN = os.path.join(ROOT, 'debug', 'train')
-	VAL = os.path.join(ROOT, 'debug', 'val')
-	TEST = os.path.join(ROOT, 'debug', 'test')
-	params['epochs'] = 10
-	params['patience'] = 2
-	N_train = params['debug_train_size']
-	N_val = params['debug_val_size']
+	params['epochs'] = 3
+	params['val_size'] = 0.25
 
 # adjust some parameters if run on cloud
 if params['cloud'] == True:
-	assert params['debug'] == False, 'Cloud version should not run in debug mode.'
-
 	OUTPUT = '/storage/kaggle_cancer_competition/output'
-	TRAIN = '/storage/kaggle_cancer_competition/temp/train'
-	VAL = '/storage/kaggle_cancer_competition/temp/val'
-	TEST = '/storage/kaggle_cancer_competition/temp/test'
+	INPUT = '/storage/kaggle_cancer_competition/input'
 
-
+# print current parameters
+print('\n-----------------------')
+print('RUNNING WITH PARAMETERS:')
+print('-DEBUG = %s' % params['debug'])
+print('-CLOUD = %s' % params['cloud'])
+print('-VALIDATION SIZE = %s' % params['val_size'])
+print('-BATCH SIZE = %s' % params['batch_size'])
+print('-EPOCHS = %s' % params['epochs'])
+print('-PATIENCE = %s' % params['patience'])
+print('-----------------------')
--- model.py
+++ model.py
@@ -1,25 +1,28 @@
+import cv2
 import datetime
 import json
 import numpy as np
 import os
 import pandas as pd
+import time
 from tqdm import tqdm
 
 from sklearn.metrics import roc_auc_score
-from tensorflow.keras.applications.inception_v3 import InceptionV3
-from tensorflow.keras.applications.inception_v3 import preprocess_input
-# from tensorflow.keras.applications.nasnet import NASNetMobile
-# from tensorflow.keras.applications.nasnet import preprocess_input
-from tensorflow.keras.callbacks import Callback
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
-from tensorflow.keras.models import load_model
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import GlobalAveragePooling2D
-from tensorflow.keras.layers import Input
-from tensorflow.keras import backend as K
+from sklearn.model_selection import train_test_split
+# from keras.applications.inception_v3 import InceptionV3
+# from keras.applications.inception_v3 import preprocess_input
+from keras.applications.nasnet import NASNetMobile
+from keras.applications.nasnet import preprocess_input
+from keras.callbacks import Callback
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+from keras.preprocessing import image
+from keras.models import Model
+from keras.models import load_model
+from keras.layers import Dense
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import Input
+from keras import backend as K
 
 from config import *
 
@@ -27,47 +30,125 @@ from config import *
 NOTE:
 - freezing InceptionV3 layers leads to weird results at test time due to
   bug linked to batch normalization layers 
-  (see http://blog.datumbox.com/the-batch-normalization-layer-of-keras-is-broken/)
+  (http://blog.datumbox.com/the-batch-normalization-layer-of-keras-is-broken/)
 
 TODO:
-- 
+- data augmentation
+- custom callback function
+- fix val acc constant across epochs
+- speed up eval and pred parts (pred by batch)
 """
-
 K.clear_session()
+
+np.random.seed(12)
+
+train_path = os.path.join(INPUT, 'train')
+test_path = os.path.join(INPUT, 'test')
+
 OUT  = os.path.join(OUTPUT, 'benchmark')
 if not os.path.exists(OUT):
     os.makedirs(OUT)
 
+file_label_map = pd.read_csv(INPUT + '/train_labels.csv')
+if params['debug'] == True:
+	file_label_map = file_label_map.iloc[0:512]
+
+f2l_train, f2l_val = train_test_split(
+	file_label_map, 
+	test_size=params['val_size'],
+	random_state=12
+	)
+
 batch_size = params['batch_size']
 epochs = params['epochs']
-steps_train = int(N_train / batch_size)
-steps_val   = N_val
+N_train = f2l_train.shape[0]
+N_val = f2l_val.shape[0]
+steps_train = N_train // batch_size
+steps_val = N_val // batch_size
 
 #################
 ### daga prep ###
 #################
 
-datagen = image.ImageDataGenerator(
-	preprocessing_function = preprocess_input
-	)
+print('\nPreparing data . . .')
 
-train_generator = datagen.flow_from_directory(
-	TRAIN,
-	target_size=(IMG_SIZE, IMG_SIZE),
-	batch_size=batch_size,
-	class_mode='binary'	)
+def load_image(file_path):
+	img = cv2.imread(os.path.join(file_path))
+	img = img[:,:,::-1]  # BGR -> RGB
+	return img
 
-val_generator = datagen.flow_from_directory(
-	VAL,
-	target_size=(IMG_SIZE, IMG_SIZE),
-	batch_size=1,
-	class_mode='binary',
-	shuffle=False
-	)
+def batch_generator(input_path, 
+					f2l, 
+					batch_size, 
+					mode='train', 
+					aug=None):
 
-#######################
-### benchmark model ###
-#######################
+	n_files = f2l.shape[0]
+	idx = 0
+	while True:
+		imgs = []
+		lbls = []
+
+		# shuffle data at each new epoch
+		if mode == 'train':
+			f2l = f2l.reindex(np.random.permutation(f2l.index))
+
+		while len(imgs) < batch_size:
+			f = f2l.iloc[idx, 0] + '.tif'
+			f_path = os.path.join(input_path, f)
+			img = load_image(f_path)
+			img = preprocess_input(img)
+			imgs.append(img)
+
+			lbl = f2l.iloc[idx, 1]
+			lbls.append(lbl)
+
+			idx += 1
+
+			if idx == n_files:
+				idx = 0  # fill batch with first files once reached the end
+				if mode == 'eval':
+					break
+
+		if aug != None:
+			pass
+
+		imgs = np.array(imgs)
+		lbls = np.array(lbls).reshape(len(lbls), 1)
+
+		yield(imgs, lbls)
+
+# initialize the generators
+train_generator = batch_generator(
+	train_path,
+	f2l_train,
+	batch_size,
+	mode='train',
+	aug=None)
+
+val_generator = batch_generator(
+	train_path,
+	f2l_val,
+	batch_size,
+	mode='eval',
+	aug=None)
+
+# test generator
+# idx = 0
+# for i, j in train_generator:
+# 	idx += 1
+# 	print(idx)
+# 	print(i.shape)
+# 	print(np.mean(i))
+# 	if idx==25:
+# 		break
+
+
+###################
+### build model ###
+###################
+
+print('\nBuilding model . . .')
 
 class auc_callback(Callback):
     def on_train_begin(self, logs={}):
@@ -106,11 +187,11 @@ callback_list = [
 	]
 
 def build_model(input_shape=(IMG_SIZE, IMG_SIZE, 3)):
-	X_input = Input(input_shape)
-	base_model = InceptionV3(
-		weights='imagenet', 
+	X_input = Input(shape=input_shape)
+	base_model = NASNetMobile(
+		input_tensor=X_input,
 		include_top=False, 
-		input_shape=input_shape
+		weights='imagenet'
 		)
 
 	# if params['debug']==True:
@@ -135,9 +216,17 @@ def build_model(input_shape=(IMG_SIZE, IMG_SIZE, 3)):
 model = build_model()
 model.summary()
 
+###################
+### train model ###
+###################
+
+print('\nTraining model . . .')
+
+ts = time.time()
+
 model.fit_generator(
     generator=train_generator,
-    steps_per_epoch=steps_train,
+    steps_per_epoch= steps_train,
     epochs=epochs,
     callbacks=callback_list,
     validation_data=val_generator,
@@ -145,70 +234,97 @@ model.fit_generator(
     verbose=1
     )
 
-# load best model (model.fit returns weights of last training epoch)
-K.clear_session()
-model = load_model(os.path.join(OUT, 'model.h5'))
+time_train = str(np.round((time.time() - ts) / 60, 2))
 
 ####################################
 ### evaluate perf validation set ###
 ####################################
 
-y_val_pred = model.predict_generator(
-	generator=val_generator,
-	steps=steps_val,
-	verbose=1
+print('\nEvaluating model . . .')
+
+ts = time.time()
+
+# load best model (model.fit returns weights of last training epoch)
+K.clear_session()
+model = load_model(os.path.join(OUT, 'model.h5'), compile=False)
+model.compile(
+	optimizer='adam', 
+	loss='binary_crossentropy', 
+	metrics=['accuracy']
 	)
 
-auc_val = roc_auc_score(val_generator.classes, y_val_pred)
-auc_val
+def predict_label(file_path):
+	img = load_image(file_path)
+	img = preprocess_input(img)
+	img = np.expand_dims(img, axis=0)
+	pred = float(model.predict(img))
+	return pred
 
+preds = []
+for id in tqdm(f2l_val['id']):
+	f_path = os.path.join(train_path, id + '.tif')
+	pred = predict_label(f_path)
+	preds.append(pred)
 
-# checks
-from sklearn.metrics import accuracy_score
-y_pred = np.array([1 if x>=0.5 else 0 for x in y_val_pred])
-accuracy_score(val_generator.classes, y_pred)
+auc_val = roc_auc_score(np.array(f2l_val['label']), preds)
 
-# model.evaluate_generator(generator=val_generator, steps=steps_val, verbose=1)
+time_eval = str(np.round((time.time() - ts) / 60, 2))
 
+model.evaluate_generator(generator=val_generator, steps=steps_val, verbose=1)
+preds = model.predict_generator(generator=val_generator, steps=steps_val, verbose=1)
+preds = np.squeeze(preds)
 
 ########################
 ### make predictions ###
 ########################
 
-test_ids = [f for f in os.listdir(TEST) if f.endswith('.tif')]
+print('\nMaking predictions . . .')
+
+ts = time.time()
+
+test_files = [f for f in os.listdir(test_path) if f.endswith('.tif')]
+if params['debug'] == True:
+	test_files = test_files[0:100]
+
 preds = []
+for f in tqdm(test_files):
+	f_path = os.path.join(test_path, f)
+	pred = predict_label(f_path)
+	preds.append(pred)	
 
-for id in tqdm(test_ids):
-	img_path = os.path.join(TEST, id)
-	img = image.load_img(img_path, target_size=(IMG_SIZE, IMG_SIZE))
-	x = image.img_to_array(img)
-	x = np.expand_dims(x, axis=0)
-	x = preprocess_input(x)
-	pred = float(model.predict(x))
-	preds.append(pred)
+time_pred = str(np.round((time.time() - ts) / 60, 2))
 
+test_ids = [f[:-4] for f in test_files]
 submission = pd.DataFrame({'id': test_ids, 'label': preds})
 
 ####################
 ### export stuff ###
 ####################
 
+print('\nSaving results . . .')
+
 def export_model(out_folder, score, model, params, submission):
-	
-	# create folder
 	score = round(score, 6)
 	today = datetime.datetime.now()
-	sub_id = today.strftime('%y%m%d') + '_' + today.strftime("%H%M") + \
+	folder_name = today.strftime('%y%m%d') + '_' + today.strftime("%H%M") + \
  		'_score_' + str(score)
-	folder = out_folder + '/' + sub_id
+	folder = os.path.join(out_folder, folder_name)
 	os.mkdir(folder)
-	print('\n---- ' + sub_id + ' ----')
 
-	# save stuff
 	model.save(os.path.join(folder, 'model.h5'))
 	with open(os.path.join(folder, 'params.json'), 'w') as f:
-    	json.dump(params, f)
+		json.dump(params, f)
 	submission.to_csv(os.path.join(folder, 'submission.csv'), index=False)
+
 
 if params['debug'] == False:
 	export_model(OUT, auc_val, model, params, submission)
+
+
+print('\n-----------------------')
+print('SUMMARY:')
+print('-VAL AUC: %s' % round(auc_val, 6))
+print('-TRAIN TIME: %s' % time_train)
+print('-EVAL TIME: %s' % time_eval)
+print('-PRED TIME: %s' % time_pred)
+print('-----------------------')
