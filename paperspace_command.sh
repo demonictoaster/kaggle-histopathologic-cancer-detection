557ebb29d3988df51b95657af1a53ef74d8da5ba
Michaël Vauthey <mickyvauthey@gmail.com>
2019-03-06 18:03:38 +0800

    added paperspace stuff to repo

---
+++ paperspace_command.sh
@@ -0,0 +1,5 @@
+paperspace-python run model.py \
+--container "docker.io/bclaes/paperspace-python:latest" \
+--machineType P4000 \
+--workspace . \
+--ignoreFiles "debug,input,output,temp,stuff,docker,old,README.md,find_duplicates.py,paperspace_command.sh"
